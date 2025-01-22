a6d0aee3662ee162ae3d9909e3486a54a7e2ab21
raul <raulnavarroepx@gmail.com>
2025-01-22 13:05:24 +0100

    La parte del backend terminada, sin control de errores.

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash
+from flask import Flask, render_template, redirect, url_for, flash, request
 from config import config
 from dotenv import load_dotenv
 from os import getenv
@@ -19,11 +19,69 @@ mysql = MySQL(app)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    cursor = mysql.connection.cursor()
+    sql = "SELECT * FROM empleados;"
+    cursor.execute(sql)
+    data = cursor.fetchall()
+    print(data)
+
+    return render_template('index.html', empleados = data)
 
 
+@app.route('/add_empleado', methods=['POST'])
+def add_empleado():
+    nombre = request.form['nombre']
+    apellido1 = request.form['apellido1']
+    apellido2 = request.form['apellido2']
+    departamento = request.form['departamento']
+    salario = request.form['salario']
+
+    if nombre and apellido1 and apellido2 and departamento and salario:
+        cursor = mysql.connection.cursor()
+        sql = "INSERT INTO empleados (nombre, apellido1, apellido2, departamento, salario) VALUES (%s, %s, %s, %s, %s)"
+        cursor.execute(sql, (nombre, apellido1, apellido2, departamento, salario)) 
+        mysql.connection.commit()
+
+        return redirect(url_for('home'))
+    
+    return redirect(url_for('home'))
 
 
+@app.route('/delete_empleado/<string:id>')
+def delete_empleado(id):
+    cursor = mysql.connection.cursor()
+    sql = "DELETE FROM empleados WHERE id = %s;"
+    cursor.execute(sql, (id,))
+    mysql.connection.commit()
+
+    return redirect(url_for('home'))
+
+
+@app.route('/edit_empleado/<string:id>')
+def edit_empleado(id):
+    cursor = mysql.connection.cursor()
+    sql = "SELECT * FROM empleados WHERE id = %s"
+    cursor.execute(sql, (id,))
+    data = cursor.fetchall()
+
+    return render_template('edit_empleado.html', empleado = data)
+
+
+@app.route('/actualizar_empleado/<string:id>', methods=['POST'])
+def actualizar_empleado(id):
+    nombre = request.form['nombre']
+    apellido1 = request.form['apellido1']
+    apellido2 = request.form['apellido2']
+    departamento = request.form['departamento']
+    salario = request.form['salario']
+
+    cursor = mysql.connection.cursor()
+    sql = "UPDATE empleados SET nombre = %s, apellido1 = %s, apellido2 = %s, departamento = %s, salario = %s WHERE id = %s;"
+    cursor.execute(sql, (nombre, apellido1, apellido2, departamento, salario, id,))
+    mysql.connection.commit()
+
+    return redirect(url_for('home'))
+    
 
 
 
